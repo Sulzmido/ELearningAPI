6449e3fd6d4ba4a7911d9063e790fecc784b9bcc
ahmed sulaiman <asulaiman@appzonegroup.com>
2020-04-27 13:28:50 +0100

    More models, more code

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"./models"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -12,8 +16,11 @@ func main() {
 
 	r.GET("/", index)
 
-	http.ListenAndServe(":8080", r)
+	r.GET("/student/:id", getStudent)
+	r.POST("/student", createStudent)
+	r.DELETE("/student/:id", deleteStudent)
 
+	http.ListenAndServe(":8080", r)
 }
 
 func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
@@ -33,4 +40,42 @@ func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(s))
+}
+
+func getStudent(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	student := models.Student{
+		ID:     p.ByName("id"),
+		Name:   "Sulaiman Ahmed",
+		Gender: "Male ",
+		Age:    21,
+	}
+
+	uj, err := json.Marshal(student)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+
+}
+
+func createStudent(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
+	// validate what is in r.Body.
+	u := models.Student{}
+	json.NewDecoder(r.Body).Decode(&u)
+	u.ID = "1"
+	// Save u.
+	uj, _ := json.Marshal(u)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func deleteStudent(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprint(w, "Write code to delete user\n")
 }
--- models/teacher.go
+++ models/teacher.go
@@ -1,7 +1,7 @@
 package models
 
-// Student model
-type Student struct {
+// Teacher model
+type Teacher struct {
 	ID     string `json:"id"`
 	Name   string `json:"name"`
 	Gender string `json:"gender"`
